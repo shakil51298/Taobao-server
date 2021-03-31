bf95cc19a52886417b9e089aa8ea1d86762a53c9
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-04-01 06:49:47 +0800

    fixed error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bptoi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
-const port = 5055
+const port = process.env.PORT || 5055 
 
 app.use(cors())
 app.use(bodyParser.json())
